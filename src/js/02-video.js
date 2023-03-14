86f3d2bec801146cf048b19cb63230d798004850
SMAILe3001 <smaile3001@gmail.com>
2023-03-14 22:38:54 +0200

    Task 2. Upgrade

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,38 +3,55 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let timeStartVideo = Number(localStorage.getItem('videoplayer-current-time'));
 
-if (timeStartVideo) {
-  player
-    .setCurrentTime(timeStartVideo)
-    .then(function (seconds) {
-      console.log(`Відео відтворюється з - ${Math.trunc(seconds)} секунди`);
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-        default:
-          break;
-      }
-    });
-}
+// -----------------СТАЛО-----------------
+
+let timeStopVideo = localStorage.getItem('videoplayer-current-time');
+let timeContiniuVideo = timeStopVideo ? Number(timeStopVideo) : 0.01;
+
+player.setCurrentTime(timeContiniuVideo);
 
 player.on(
   'timeupdate',
-  throttle(function () {
-    currentPlaybackTime();
+  throttle(function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
   }, 1000)
 );
 
-function currentPlaybackTime() {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    })
-    .catch(function (error) {
-      console.log('Что то пошло не так');
-    });
-}
+// -----------------БУЛО-----------------
+
+// let timeStartVideo = Number(localStorage.getItem('videoplayer-current-time'));
+
+// if (timeStartVideo) {
+//   player
+//     .setCurrentTime(timeStartVideo)
+//     .then(function (seconds) {
+//       console.log(`Відео відтворюється з - ${Math.trunc(seconds)} секунди`);
+//     })
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
+//         default:
+//           break;
+//       }
+//     });
+// }
+
+// player.on(
+//   'timeupdate',
+//   throttle(function () {
+//     currentPlaybackTime();
+//   }, 1000)
+// );
+
+// function currentPlaybackTime() {
+//   player
+//     .getCurrentTime()
+//     .then(function (seconds) {
+//       localStorage.setItem('videoplayer-current-time', seconds);
+//     })
+//     .catch(function (error) {
+//       console.log('Что то пошло не так');
+//     });
+// }
