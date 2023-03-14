b96803fa659c049760e4519e513b5d5070c08dbe
SMAILe3001 <smaile3001@gmail.com>
2023-03-14 20:28:37 +0200

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+let unfinishedFeedback = {};
+
+formEl.addEventListener(
+  'input',
+  throttle(e => {
+    unfinishedFeedback[e.target.name] = e.target.value;
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(unfinishedFeedback)
+    );
+  }, 500)
+);
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  console.log(unfinishedFeedback);
+
+  localStorage.removeItem('feedback-form-state');
+  e.currentTarget.reset();
+});
+
+function recallFeedback(evt) {
+  evt = localStorage.getItem('feedback-form-state');
+  if (!evt) {
+    return;
+  }
+
+  unfinishedFeedback = JSON.parse(evt);
+
+  Object.entries(unfinishedFeedback).forEach(([name, value]) => {
+    formEl.elements[name].value = value;
+  });
+}
+
+recallFeedback();
