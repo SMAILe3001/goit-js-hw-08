f42b0e6a8ceaa5528c69c72e0a317cdcf515b701
SMAILe3001 <smaile3001@gmail.com>
2023-03-14 16:37:18 +0200

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsGeneration(galleryItems));
+
+function galleryItemsGeneration(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, description, original }) => `
+            <div class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img
+					class="gallery__image"
+					src="${preview}"
+					alt="${description}"
+					/>
+				</a>
+			</div>`
+    )
+    .join(' ');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
